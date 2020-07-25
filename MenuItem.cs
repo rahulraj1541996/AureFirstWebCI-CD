26579a904018d315122487d8ca4e34f629744350
rahulraj1541996 <60652862+rahulraj1541996@users.noreply.github.com>
2020-07-25 10:47:11 +0530

    Add files via upload

---
+++ MenuItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenuItemListing
+{
+    public class MenuItem
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Display(Name ="Free Delivery")]
+        public bool freeDelivery { get; set; }
+        [Required]
+        public int Price { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime dateOfLaunch { get; set; }
+        public bool Active { get; set; }
+    }
+}
